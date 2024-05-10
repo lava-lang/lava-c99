4e47875ec75b0921c49b6b9a56444833db1a8fb4
Muramasa <haydenkilloh@gmail.com>
2024-05-10 17:12:42 +0100

    cleanup some code

--- include/hashmap.h
+++ include/hashmap.h
@@ -12,24 +12,23 @@ typedef struct hash_table_entry {
     struct hash_table_entry* next;
 } hash_table_entry;
 
-typedef struct hash_table
-{
+typedef struct hash_table {
     size_t size;
     struct hash_table_entry **table;
 } hash_table;
 
-struct hash_table* init_hash_table(size_t number_of_rows)
-{
-    if(!number_of_rows)
+struct hash_table* init_hash_table(size_t number_of_rows) {
+    if (!number_of_rows) {
         return NULL;
+    }
 
     struct hash_table *new_table=(struct hash_table*)malloc(sizeof(struct hash_table)); //bartender, one hashtable please
-    if(NULL==new_table)
+    if (NULL==new_table) {
         return NULL;
+    }
 
     new_table->table=(struct hash_table_entry **) malloc(number_of_rows * sizeof(struct hash_table_entry *));
-    if(NULL == (new_table->table) )
-    {
+    if (NULL == (new_table->table)) {
         free(new_table);
         return NULL;
     }
@@ -40,11 +39,9 @@ struct hash_table* init_hash_table(size_t number_of_rows)
 }
 
 //The simplest hash function. Converts 'key' string into a value in range 0- (number_of_rows-1)
-size_t hashFunction(const char *key, size_t number_of_rows)
-{
+size_t hashFunction(const char *key, size_t number_of_rows) {
     unsigned sum=0;
-    while('\0' != *key )
-    {
+    while('\0' != *key) {
         sum+=key[0]; //it's okay if sum overflows
         key++;
     }
@@ -81,15 +78,12 @@ int insert(struct hash_table *the_hash_table, char* key, void* value) {
 }
 
 //returns 0 on success
-int find(struct hash_table *the_hash_table, char *key, struct hash_table_entry* ret_val)
-{
+int find(struct hash_table *the_hash_table, char *key, struct hash_table_entry* ret_val) {
     //head to the current row
     struct hash_table_entry *head_ll=the_hash_table->table[hashFunction(key, the_hash_table->size)];
 
-    while(NULL != head_ll)
-    {
-        if(0==strcmp(head_ll->key,key))
-        {
+    while(NULL != head_ll) {
+        if(0==strcmp(head_ll->key,key)) {
             //found match! Make a copy and transfer over value
             if (ret_val != NULL) {
                 memcpy(ret_val, head_ll, sizeof(struct hash_table_entry));
@@ -102,15 +96,13 @@ int find(struct hash_table *the_hash_table, char *key, struct hash_table_entry* 
 }
 
 //returns 0 on success
-int delete(struct hash_table *the_hash_table, char *key)
-{
+int delete(struct hash_table *the_hash_table, char *key) {
     //head to the current row
     struct hash_table_entry *head_ll=the_hash_table->table[hashFunction(key, the_hash_table->size)];
 
-    if(NULL == head_ll)
+    if (NULL == head_ll)
         return -1;
-    if(0==strcmp(head_ll->key, key))
-    {
+    if (0==strcmp(head_ll->key, key)) {
         //head is the element to be deleted
         the_hash_table->table[hashFunction(key, the_hash_table->size)]=head_ll->next;
         free(head_ll->key);
@@ -118,10 +110,8 @@ int delete(struct hash_table *the_hash_table, char *key)
         free(head_ll);
         return 0;
     }
-    while(NULL!=(head_ll->next))
-    {
-        if(0==strcmp(head_ll->next->key, key))
-        {
+    while (NULL!=(head_ll->next)) {
+        if (0==strcmp(head_ll->next->key, key)) {
             //found element, bypass this element in the LL
             struct hash_table_entry *tmp=head_ll->next;
             head_ll->next=tmp->next;
@@ -136,16 +126,13 @@ int delete(struct hash_table *the_hash_table, char *key)
     return -1; //didnt find key
 }
 
-void deleteTable(struct hash_table *my_table)
-{
+void deleteTable(struct hash_table *my_table) {
     //for each entry, delete the entry linked list
     size_t i=0;
     struct hash_table_entry *head_ll, *temp;
-    while(i< my_table->size)
-    {
+    while(i< my_table->size) {
         head_ll=my_table->table[i];
-        while(NULL != head_ll)
-        {
+        while(NULL != head_ll) {
             temp=head_ll->next;
             free(head_ll->key);
             free(head_ll->value);
